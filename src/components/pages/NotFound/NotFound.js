cdfb889fdbbc57af485bf2688973da370c465a71
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-06-24 12:06:46 +0200

    Create simple page NotFound

---
+++ src/components/pages/NotFound/NotFound.js
@@ -0,0 +1,25 @@
+import styles from './NotFound.module.scss';
+
+const NotFound = () => {
+    return (
+        <div>
+            <header>
+                <span>Waiter.app</span>
+
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <main>
+                <h1>Not Found</h1>
+            </main>
+
+            <footer>
+                <p>Copyright @ PizzeriaApp 2022</p>
+            </footer>
+        </div>
+    );
+}
