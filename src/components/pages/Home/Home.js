2ae96003e61afcd26e8b608a8e60a29d2263e07e
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-06-27 07:16:40 +0200

    Custom main page and style it

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,56 +1,43 @@
 import styles from './Home.module.scss';
+import './Home.scss';
 
 const Home = () => {
     return (
         <div>
-            <header>
-                <span>Waiter.app</span>
-
-                <nav>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                    </ul>
-                </nav>
-            </header>
-
             <main>
                 <h1>All tables</h1>
 
-                <ul>
-                    <li>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
                         <div>
                             <h2>Table 1</h2>
                             <p><b>Status:</b> Reserved</p>
-                            <button>Show more</button>
                         </div>
+                        <button className="btn btn-primary">Show more</button>
                     </li>
-                    <li>
+                    <li className="list-group-item">
                         <div>
                             <h2>Table 2</h2>
                             <p><b>Status:</b> Free</p>
-                            <button>Show more</button>
                         </div>
+                        <button className="btn btn-primary">Show more</button>
                     </li>
-                    <li>
+                    <li className="list-group-item">
                         <div>
                             <h2>Table 3</h2>
                             <p><b>Status:</b> Busy</p>
-                            <button>Show more</button>
                         </div>
+                        <button className="btn btn-primary">Show more</button>
                     </li>
-                    <li>
+                    <li className="list-group-item">
                         <div>
                             <h2>Table 4</h2>
                             <p><b>Status:</b> Cleaning</p>
-                            <button>Show more</button>
                         </div>
+                        <button className="btn btn-primary">Show more</button>
                     </li>
                 </ul>
             </main>
-
-            <footer>
-                <p>Copyright @ PizzeriaApp 2022</p>
-            </footer>
         </div>
     );
 }
