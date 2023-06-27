7e80d0bc50eee10343662b61bb73b2934e34ad7d
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-06-27 10:30:44 +0200

    a commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,26 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import initialState from './initialState';
-import tablesReducer from './tablesRedux';
+import { createStore } from 'redux';
+import initialState from './initialState.js';
 
-const subreducers = {
-    tablesReducer
-}
+//selectors
+export const getAllTables = state => state.tables;
+export const getTableById = (state, id) => state.tables.filter(table => table.id == id);
 
-const reducer = combineReducers(subreducers);
+// action creators
+export const updateTables = payload => ({ type: 'UPDATE_TABLES', payload });
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE_TABLES':
+      return [...action.payload]
+    default:
+      return state;
+  }
+};
+
 const store = createStore(
   reducer,
   initialState,
-
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
-  )
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
