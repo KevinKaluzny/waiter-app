1b1ba83e937131513a018456004a0cd542482915
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-06-27 08:19:55 +0200

    Custom table page and style it

--- src/components/pages/Table/Table.js
+++ src/components/pages/Table/Table.js
@@ -1,46 +1,33 @@
 import styles from './Table.module.scss';
+import './Table.scss';
 
 const Table = () => {
     return (
         <div>
-            <header>
-                <span>Waiter.app</span>
-
-                <nav>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                    </ul>
-                </nav>
-            </header>
-
             <main>
                 <h1>Table 1</h1>
 
                 <form>
-                    <div>
+                    <div class="input-group mb-3">
                         <label for="status"><b>Status:</b></label>
-                        <select id="status">
-                            <option value="busy">Busy</option>
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected value="busy">Busy</option>
                             <option value="reserved">Reserved</option>
                             <option value="free">Free</option>
                             <option value="cleaning">Cleaning</option>
                         </select>
                     </div>
                     <div>
-                        <label for="people"><b>Status:</b></label>
-                        <input type="number" id="people" /> / <input type="number" />
+                        <label for="people" class="form-label"><b>People:</b></label>
+                        <input class="form-control" type="number" min="0" max="10" id="people" /> / <input class="form-control" type="number" min="0" max="10" />
                     </div>
                     <div>
-                        <label for="bill"><b>Status:</b></label>
-                        $ <input type="number" id="bill" />
+                        <label for="bill"><b>Bill:</b></label>
+                        $ <input class="form-control" type="number" min="0" id="bill" />
                     </div>
-                    <button>Update</button>
+                    <button class="btn btn-primary">Update</button>
                 </form>
             </main>
-
-            <footer>
-                <p>Copyright @ PizzeriaApp 2022</p>
-            </footer>
         </div>
     );
 }
